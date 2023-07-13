b91bb2f661edebe9fd0a5cc608beec209f85d8ae
Arthur Simon <developarvin@gmail.com>
2023-07-13 11:59:27 +0800

    Add sleep to api endpoint

--- src/app/api/analytics/route.ts
+++ src/app/api/analytics/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from 'next/server'
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
  
 export async function POST() {
   const data = { 'foo': 'bar'}
- 
+  await sleep(3000)
   return NextResponse.json(data)
 }
